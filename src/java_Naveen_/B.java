36f6f4dceb9ca487f32860de96109427dcfe58f9
Jey <jey@Jeys-MBP.home>
2020-12-21 22:37:46 -0500

    new code new class about color

--- src/java_Naveen_/B.java
+++ src/java_Naveen_/B.java
@@ -4,13 +4,17 @@ public class B {
 
 	public static void main(String[] args) {
 
-System.out.println("B----main mathed ");
-B.main(args);
-		
+//System.out.println("B----main mathed ");
+//B.main(args);
 
 // this is the stack over flow exception 
 
+		String NaveenAvailable = "NaveenAutomationLabs";
 		
-	}
-
-}
+	
+		
+		if (NaveenAvailable != null) {	
+			System.err.println("signUPforGettingAmazingStuff in  "+NaveenAvailable);
+			}
+		}
+     }
