de58372984e962f6502bc13f0b598ce535189478
Jey <jey@Jeys-MacBook-Pro.local>
2021-10-23 09:39:15 -0400

    new fun added

---
+++ src/javaCodingChallenge/Fun.java
@@ -0,0 +1,15 @@
+package javaCodingChallenge;
+
+public class Fun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fun = null;
+		if(fun == null) {
+			System.out.println("lets fun because null fun is the best fun");
+
+		}
+		
+	}
+
+}
